d6ad4ca958be0e62bd2f4b1e563b8b6f9f6030ac
yam-net <myam0118@gmail.com>
2019-01-21 23:02:15 +0900

    rewrite code test according to the book

--- code/code_test.go
+++ code/code_test.go
@@ -1,26 +1,37 @@
 package code
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestMake(t *testing.T) {
-	testCases := []struct {
+	tests := []struct {
+		desc     string
 		opcode   Opcode
 		operands []int
 		expected []byte
 	}{
 		{
-			opcode: OpConstant,
+			desc:     "OpConstant",
+			opcode:   OpConstant,
 			operands: []int{65534},
 			expected: []byte{byte(OpConstant), 255, 254},
 		},
 	}
 
-	for _, tC := range testCases {
-        instruction := Make(tC.opcode, tC.operands...)
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			instruction := Make(tt.opcode, tt.operands...)
 
-        assert.Equal(t, instruction, tC.expected)
+			if len(instruction) != len(tt.expected) {
+				t.Fatalf("instruction has wrong length. expected=%q, actual=%q", tt.expected, instruction)
+			}
+
+			for i, e := range tt.expected {
+				if instruction[i] != e {
+					t.Fatalf("instruction has wrong byte at %d. expected=%d, actual=%d", i, e, instruction[i])
+				}
+			}
+		})
 	}
 }
