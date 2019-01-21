369be8b41a60930a9e8cef5ed9539b1662cbff51
yam-net <myam0118@gmail.com>
2019-01-22 00:24:52 +0900

    add code to emit instruction for integer literal

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -19,11 +19,34 @@ type Compiler struct {
 func New() *Compiler {
 	return &Compiler{
 		instructions: code.Instructions{},
-		constants: []object.Object{},
+		constants:    []object.Object{},
 	}
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, statement := range node.Statements {
+			if err := c.Compile(statement); err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		if err := c.Compile(node.Expression); err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		if err := c.Compile(node.Left); err != nil {
+			return err
+		}
+		if err := c.Compile(node.Right); err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
 }
 
@@ -32,4 +55,21 @@ func (c *Compiler) ByteCode() *ByteCode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(opcode code.Opcode, operands ...int) int {
+	ins := code.Make(opcode, operands...)
+	position := c.addNewInstruction(ins)
+	return position
+}
+
+func (c *Compiler) addNewInstruction(ins []byte) int {
+	position := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return position
 }
